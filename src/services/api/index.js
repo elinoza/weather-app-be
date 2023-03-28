a8a5614ca52b3b18d8dcdd37d2d9bd7fcf538b7c
elinoza <hillcakmak@gmail.com>
2023-03-28 19:12:41 +0300

    units metric added to weather api endpoint

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -7,7 +7,8 @@ apiRouter.get("/:geoCoo", async (req, res, next) => {
   try {
     const url = process.env.WEATHER_APP_URL;
     const key = process.env.WEATHER_APP_KEY;
-    let query = `forecast?${req.params.geoCoo}&appid=${key}`;
+    const units="metric"
+    let query = `forecast?${req.params.geoCoo}&appid=${key}&units=${units}`;
     console.log(req.params.geoCoo)
     let response = await fetch(url + query);
     let weather = await response.json();
@@ -26,6 +27,7 @@ try {
   const url = process.env.WEATHER_APP_CONVERTER_URL;
   const key = process.env.WEATHER_APP_KEY;
   const limit= 10;
+ 
   let query = `direct?q=${req.params.selectedCity}&limit=${limit}&appid=${key}`;
   console.log(req.params.selectedCity,query,url,limit)
   let response = await fetch (url+query)
