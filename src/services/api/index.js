a8e882978523e5b4fb2ed819a359bf964877f47d
elinoza <hillcakmak@gmail.com>
2023-03-28 16:00:43 +0300

    converter api imlemented

--- src/services/api/index.js
+++ src/services/api/index.js
@@ -11,12 +11,31 @@ apiRouter.get("/:geoCoo", async (req, res, next) => {
     console.log(req.params.geoCoo)
     let response = await fetch(url + query);
     let weather = await response.json();
-    console.log(response,weather)
+
     res.send(weather);
   } catch (error) {
     next(error);
   }
 });
+
+apiRouter.get("/geo/:selectedCity",async(req,res,next)=>{
+
+try {
+  console.log(process.env.WEATHER_APP_CONVERTER_URL,process.env.WEATHER_APP_KEY)
+  
+  const url = process.env.WEATHER_APP_CONVERTER_URL;
+  const key = process.env.WEATHER_APP_KEY;
+  const limit= 10;
+  let query = `direct?q=${req.params.selectedCity}&limit=${limit}&appid=${key}`;
+  console.log(req.params.selectedCity,query,url,limit)
+  let response = await fetch (url+query)
+  let weather = await response.json()
+  res.send(weather)
+} catch (error) {
+  next(error)
+}
+
+})
 
 
 
